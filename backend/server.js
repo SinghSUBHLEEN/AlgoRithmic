1d0741c0c16f01a6b4055f4c19aa89040f04766d
SinghSUBHLEEN <singhcherry9563@gmail.com>
2023-08-01 18:12:25 +0530

    minor_changes_temp

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-require('../process_env');
+require("dotenv").config();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookie = require("cookie-parser");
